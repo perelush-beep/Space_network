43605599d9375d018a97a719424658a289395ebd
Aharom_Perel <perelush@gmail.com>
2026-01-27 11:48:03 +0200

    Added a critical errors that cannot be corrected by a retry

--- satellite.py
+++ satellite.py
@@ -1,7 +1,9 @@
 import time
 from abc import ABC
+from sys import exception
 
-from space_network_lib import SpaceEntity, Packet, TemporalInterferenceError, DataCorruptedError, SpaceNetwork
+from space_network_lib import SpaceEntity, Packet, TemporalInterferenceError, DataCorruptedError, SpaceNetwork, \
+    LinkTerminatedError, OutOfRangeError
 
 
 class Satellite(SpaceEntity):
@@ -12,7 +14,8 @@ class Satellite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"Name: {self.name}, Received: {packet}")
 
-
+class BrokenConnectionError(Exception):
+    pass
 def attempt_transmission(packet: Packet, level):
     net = SpaceNetwork(level=level)
     while True:
@@ -20,10 +23,16 @@ def attempt_transmission(packet: Packet, level):
             net.send(packet)
             return
         except TemporalInterferenceError:
-            print("...interference waiting")
+            print("interference waiting...")
             time.sleep(2)
         except DataCorruptedError:
             print("data corrupted, retrying...")
+        except LinkTerminatedError:
+            print("link lost")
+            raise BrokenConnectionError()
+        except OutOfRangeError:
+            print("target out of range")
+            raise BrokenConnectionError()
 
 
 
