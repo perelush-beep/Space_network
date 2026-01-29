3f02252de13b89978fa4989065484072c0de9a28
Aharom_Perel <perelush@gmail.com>
2026-01-29 16:00:50 +0200

    added an  option to send packets through a longer distanceswith encapsulation

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from satellite import *
 from space_network_lib import *
 
-
+earth = Earth("earth", 0)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 p1 = Packet("can you hear me major Tom?", sat1, sat2)
+p_final = Packet("Hello from earth", sat1, sat2)
+p_earth_to_sat1 = RelayPacket(p_final, earth, sat1)
+
 try:
-    attempt_transmission(p1,3)
+    attempt_transmission(p_earth_to_sat1)
 except BrokenConnectionError:
     print("Transmission failed")
+
